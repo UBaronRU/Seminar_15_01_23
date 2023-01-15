4abb0474a3748f3c60123235c84b1f41e099b02f
UBaronRU <ubaron@mail.ru>
2023-01-15 14:59:28 +0300

    второй пример

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Определить номер четверти плоскости, в которой находится точка с координатами Х и У, причем X ≠ 0 и Y ≠ 0
+int Num1; // Координата х
+int Num2; //  Координата y
+Console.Write("Введите Координата х  : ");
+Num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите Координата y: ");
+Num2 = int.Parse(Console.ReadLine());
+if (Num1 > 0 & Num2 > 0) Console.WriteLine ("1 четверть");
+if (Num1 > 0 & Num2 < 0) Console.WriteLine ("2 четверть");
+if (Num1 < 0 & Num2 > 0) Console.WriteLine ("4 четверть");
+if (Num1 < 0 & Num2 < 0) Console.WriteLine ("3 четверть");
