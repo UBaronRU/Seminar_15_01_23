ac7c916a6f69461ec5dca8fb94da14fb104e7f82
UBaronRU <ubaron@mail.ru>
2023-01-15 15:08:08 +0300

    второй пример 2

--- Program.cs
+++ Program.cs
@@ -5,7 +5,30 @@ Console.Write("Введите Координата х  : ");
 Num1 = int.Parse(Console.ReadLine());
 Console.Write("Введите Координата y: ");
 Num2 = int.Parse(Console.ReadLine());
-if (Num1 > 0 & Num2 > 0) Console.WriteLine ("1 четверть");
-if (Num1 > 0 & Num2 < 0) Console.WriteLine ("2 четверть");
-if (Num1 < 0 & Num2 > 0) Console.WriteLine ("4 четверть");
-if (Num1 < 0 & Num2 < 0) Console.WriteLine ("3 четверть");
+if (Num1 > 0 && Num2 > 0) Console.WriteLine ("1 четверть");
+if (Num1 > 0 && Num2 < 0) Console.WriteLine ("2 четверть");
+if (Num1 < 0 && Num2 > 0) Console.WriteLine ("4 четверть");
+if (Num1 < 0 && Num2 < 0) Console.WriteLine ("3 четверть");
+// Второй способ
+if (Num1 > 0 ) 
+{
+    if (Num2 > 0 ) 
+    {
+        Console.WriteLine ("1 четверть");
+    }
+    else
+    {
+        Console.WriteLine ("2 четверть");
+    }
+}
+else
+{
+ if (Num2 > 0 ) 
+    {
+        Console.WriteLine ("4 четверть");
+    }
+    else
+    {
+        Console.WriteLine ("3 четверть");
+    }
+}
